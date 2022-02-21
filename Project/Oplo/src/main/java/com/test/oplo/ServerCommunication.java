0e58cf980f3e13926f214a4260f9b9083f48389b
Champagne-Champomy <gaetan.ignarra.pro@gmail.com>
2022-02-21 21:44:06 +0100

    ok

---
+++ Project/Oplo/src/main/java/com/test/oplo/ServerCommunication.java
@@ -0,0 +1,75 @@
+package com.test.oplo;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ *
+ * @author Clément
+ */
+public class ServerCommunication 
+{ 
+    public String sendGetRequest(String urlIn)
+    {
+        try
+        {
+            URL myurl = new URL(urlIn);
+            HttpURLConnection httpUrl =(HttpURLConnection)myurl.openConnection();
+            httpUrl.setRequestMethod("GET");
+            String line = "";
+            InputStreamReader reder = new InputStreamReader(httpUrl.getInputStream());
+            BufferedReader bufferReader = new BufferedReader(reder);
+            
+            StringBuilder response = new StringBuilder();
+            
+            while((line = bufferReader.readLine()) != null)
+            {
+                response.append(line);
+            }
+            bufferReader.close();
+            
+            return response.toString();
+        }
+        catch(Exception e)
+        {
+            return "Error while making http GET request on " + urlIn;
+        }
+    }
+    
+    public String sendPostRequest(String urlIn, String param)
+    {
+         try
+         {
+            URL myurl = new URL(urlIn);
+            HttpURLConnection httpUrl =(HttpURLConnection)myurl.openConnection();
+            httpUrl.setRequestMethod("POST");
+            httpUrl.setDoOutput(true);
+            
+            OutputStream output = httpUrl.getOutputStream();
+            output.write(param.getBytes());
+            output.flush();
+            output.close();
+            
+            String line;
+            InputStreamReader rder = new InputStreamReader(httpUrl.getInputStream());
+            BufferedReader bufferedReader = new BufferedReader(rder);
+            
+            StringBuilder response = new StringBuilder();
+            
+            while((line = bufferedReader.readLine()) != null)
+            {
+                response.append(line);
+            }
+            bufferedReader.close();
+            
+            return response.toString();
+        }
+        catch(Exception e)
+        {
+            return "Error while making http POST request on " + urlIn;
+        }
+    }
+}
